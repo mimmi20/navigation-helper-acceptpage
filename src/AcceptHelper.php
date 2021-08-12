f1a3ee38e869d62720e0fae7b46346212bcdc595
Thomas Müller <mimmi20@live.de>
2021-08-12 16:23:05 +0200

    initial commit

---
+++ src/AcceptHelper.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * This file is part of the mimmi20/navigation-helper-acceptpage package.
+ *
+ * Copyright (c) 2020-2021, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace Mimmi20\NavigationHelper\Accept;
+
+use Laminas\Navigation\Page\AbstractPage;
+use Laminas\Permissions\Acl\Acl;
+use Laminas\Permissions\Acl\Resource\ResourceInterface;
+use Mezzio\GenericAuthorization\AuthorizationInterface;
+use Mezzio\Navigation\Page\PageInterface;
+
+final class AcceptHelper implements AcceptHelperInterface
+{
+    /**
+     * Authorization to use when iterating pages
+     *
+     * @var Acl|AuthorizationInterface|null
+     */
+    private $authorization;
+
+    /**
+     * Whether invisible items should be rendered by this helper
+     */
+    private bool $renderInvisible = false;
+
+    /**
+     * Authorization role to use when iterating pages
+     */
+    private ?string $role = null;
+
+    /**
+     * @param Acl|AuthorizationInterface|null $authorization
+     */
+    public function __construct(
+        $authorization,
+        bool $renderInvisible,
+        ?string $role
+    ) {
+        $this->authorization   = $authorization;
+        $this->renderInvisible = $renderInvisible;
+        $this->role            = $role;
+    }
+
+    /**
+     * Determines whether a page should be accepted when iterating
+     *
+     * Rules:
+     * - If a page is not visible it is not accepted, unless RenderInvisible has
+     *   been set to true
+     * - If $useAuthorization is true (default is true):
+     *      - Page is accepted if Authorization returns true, otherwise false
+     * - If page is accepted and $recursive is true, the page
+     *   will not be accepted if it is the descendant of a non-accepted page
+     *
+     * @param AbstractPage|PageInterface $page      page to check
+     * @param bool                       $recursive [optional] if true, page will not be
+     *                                              accepted if it is the descendant of
+     *                                              a page that is not accepted. Default
+     *                                              is true
+     *
+     * @return bool Whether page should be accepted
+     */
+    public function accept($page, bool $recursive = true): bool
+    {
+        if (!$page->isVisible(false) && !$this->renderInvisible) {
+            return false;
+        }
+
+        $accept    = true;
+        $resource  = $page->getResource();
+        $privilege = $page->getPrivilege();
+
+        if ($resource instanceof ResourceInterface) {
+            $resource = $resource->getResourceId();
+        }
+
+        if (null !== $resource || null !== $privilege) {
+            if ($this->authorization instanceof AuthorizationInterface) {
+                $accept = $this->authorization->isGranted($this->role, $resource, $privilege);
+            }
+
+            if ($this->authorization instanceof Acl) {
+                $accept = $this->authorization->isAllowed($this->role, $resource, $privilege);
+            }
+        }
+
+        if ($accept && $recursive) {
+            $parent = $page->getParent();
+
+            if ($parent instanceof PageInterface) {
+                $accept = $this->accept($parent, true);
+            }
+        }
+
+        return $accept;
+    }
+
+    /**
+     * @return Acl|AuthorizationInterface|null
+     */
+    public function getAuthorization()
+    {
+        return $this->authorization;
+    }
+
+    public function getRenderInvisible(): bool
+    {
+        return $this->renderInvisible;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+}
---
+++ tests/AcceptHelperFactoryTest.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * This file is part of the mimmi20/navigation-helper-acceptpage package.
+ *
+ * Copyright (c) 2020-2021, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace Mimmi20Test\NavigationHelper\Accept;
+
+use Interop\Container\ContainerInterface;
+use Mezzio\GenericAuthorization\AuthorizationInterface;
+use Mimmi20\NavigationHelper\Accept\AcceptHelper;
+use Mimmi20\NavigationHelper\Accept\AcceptHelperFactory;
+use PHPUnit\Framework\Exception;
+use PHPUnit\Framework\TestCase;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+
+use function assert;
+
+final class AcceptHelperFactoryTest extends TestCase
+{
+    private AcceptHelperFactory $factory;
+
+    protected function setUp(): void
+    {
+        $this->factory = new AcceptHelperFactory();
+    }
+
+    /**
+     * @throws Exception
+     * @throws InvalidArgumentException
+     */
+    public function testInvocationWithoutOptions(): void
+    {
+        $container = $this->getMockBuilder(ContainerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $container->expects(self::never())
+            ->method('get');
+
+        assert($container instanceof ContainerInterface);
+        $helper = ($this->factory)($container, '');
+
+        self::assertInstanceOf(AcceptHelper::class, $helper);
+
+        self::assertNull($helper->getAuthorization());
+        self::assertNull($helper->getRole());
+        self::assertFalse($helper->getRenderInvisible());
+    }
+
+    /**
+     * @throws Exception
+     * @throws InvalidArgumentException
+     */
+    public function testInvocationWithOptions(): void
+    {
+        $container = $this->getMockBuilder(ContainerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $container->expects(self::never())
+            ->method('get');
+
+        $auth            = $this->createMock(AuthorizationInterface::class);
+        $renderInvisible = true;
+        $role            = 'test-role';
+
+        assert($container instanceof ContainerInterface);
+        $helper = ($this->factory)(
+            $container,
+            '',
+            [
+                'authorization' => $auth,
+                'renderInvisible' => $renderInvisible,
+                'role' => $role,
+            ]
+        );
+
+        self::assertInstanceOf(AcceptHelper::class, $helper);
+
+        self::assertSame($auth, $helper->getAuthorization());
+        self::assertSame($role, $helper->getRole());
+        self::assertTrue($helper->getRenderInvisible());
+    }
+
+    /**
+     * @throws Exception
+     * @throws InvalidArgumentException
+     */
+    public function testInvocationWithOptions2(): void
+    {
+        $container = $this->getMockBuilder(ContainerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $container->expects(self::never())
+            ->method('get');
+
+        $auth            = 'invalid-auth';
+        $renderInvisible = '1';
+        $role            = null;
+
+        assert($container instanceof ContainerInterface);
+        $helper = ($this->factory)(
+            $container,
+            '',
+            [
+                'authorization' => $auth,
+                'renderInvisible' => $renderInvisible,
+                'role' => $role,
+            ]
+        );
+
+        self::assertInstanceOf(AcceptHelper::class, $helper);
+
+        self::assertNull($helper->getAuthorization());
+        self::assertNull($helper->getRole());
+        self::assertTrue($helper->getRenderInvisible());
+    }
+
+    /**
+     * @throws Exception
+     * @throws InvalidArgumentException
+     */
+    public function testInvocationWithOptionsRuleNotString(): void
+    {
+        $container = $this->getMockBuilder(ContainerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $container->expects(self::never())
+            ->method('get');
+
+        $auth            = 'invalid-auth';
+        $renderInvisible = '1';
+        $role            = [];
+
+        assert($container instanceof ContainerInterface);
+        $helper = ($this->factory)(
+            $container,
+            '',
+            [
+                'authorization' => $auth,
+                'renderInvisible' => $renderInvisible,
+                'role' => $role,
+            ]
+        );
+
+        self::assertInstanceOf(AcceptHelper::class, $helper);
+
+        self::assertNull($helper->getAuthorization());
+        self::assertNull($helper->getRole());
+        self::assertTrue($helper->getRenderInvisible());
+    }
+}
